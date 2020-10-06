b3006b097f529581bef8f6b67d90059054b76d16
Isaac ramirez velez <ramirezisaac.1518@gmail.com>
2020-10-06 03:17:24 -0600

    Todo listo

--- src/controllers/resultados.js
+++ src/controllers/resultados.js
@@ -3,38 +3,30 @@ const alumnos = require("../models/alumnos");
 const Alumno = require("../models/alumnos")
 
 async function TraerAlumnos(req,res){
-    const curp=req.params.curp
+    const rawCurp=req.params.curp
+    const curp = rawCurp.toLowerCase().trim()
+
+    
     try {
         const [resultado] = await Alumno.find({curp})
+        
         if(resultado){
-            if(resultado.aceptado){
-                res.status(200).json(
-                    {
-                        "titulo": "Bienvenido al cbtis 128 la casa de los jaguares",
-                        "mensaje": "Felicidades, tu examen due evaluado y fuiste aceptado en la institucion",
-                        resultado
-                    })
-            }
-            if(!resultado.aceptado){
-                res.status(200).json(
-                    {
-                        "titulo": "No fuiste aceptado",
-                        "mensaje": "Puedes encontrasr mas informacion aqui",
-                        "link": "https://www.x.com",
-                        resultado
-
-                    })
-            }
+                res.status(200).json(resultado)
         }
-        if(!resultado){
+        if(!resultado ){
             res.status(404).json(
                 {
-                    "titulo":"Error",
+                    "curp":curp,
+                    "titulo":"error-404",
                     "mensaje": "La curp que ha introducido esta mal escrita o esa curp no esta registrada, favor de verificarla"
                 })
         }
     } catch (error) {
-        res.status(500).json({"mensaje": "ha ocurrido un error en el servidor: "+error})
+        res.status(500).json(
+            {
+                "titulo":"error-500",
+                "mensaje": "ha ocurrido un error en el servidor: "+error
+            })
     }
 }
   
